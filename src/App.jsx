2f99bbdefeb39771feea19b548005c2df6e0b5db
Sameer <sameer.btech.cse@gmail.com>
2025-07-10 12:05:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+//impoitng components
+import Weather from './components/Weather'
+
+export default function App() {
+  return (
+    <div className='app'>
+     <Weather/>
+     <footer>Developed by @   sameer</footer>
+      </div>
+    
+  )
+}
